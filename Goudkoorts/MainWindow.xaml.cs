cf5c309a408173ebc24e7039d44cf376324e8dc2
Ralf Hendriks <rjhendri@avans.nl>
2016-10-24 13:22:43 +0200

    fixed movable objects

--- Goudkoorts/MainWindow.xaml.cs
+++ Goudkoorts/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,7 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Goudkoorts
 {
@@ -21,10 +22,12 @@ namespace Goudkoorts
     public partial class MainWindow : Window
     {
         private GameController _game;
+        private Dictionary<String, BitmapImage> _switches = new Dictionary<string, BitmapImage>();
 
         public MainWindow()
         {
             InitializeComponent();
+            initSwitches();
             initGrid(12,8);
             _game = new GameController(this);
         }
@@ -33,12 +36,36 @@ namespace Goudkoorts
         {
             Image img = new Image();
             img.Visibility = Visibility.Visible;
-            img.Source = new BitmapImage(new Uri(@"Assets/Images/" + tile.GetType().ToString().Split('.').Last() + tile.Direction + ".png", UriKind.Relative));
+            string type = tile.GetType().ToString().Split('.').Last();
+            img.Source = new BitmapImage(new Uri(@"Assets/Images/" + type + tile.Direction + ".png", UriKind.Relative));
+            if(type == "SwitchTile")
+            {
+                img.MouseEnter += Img_MouseEnter;
+                img.MouseDown += Img_MouseDown;
+            }
             img.Width = 50;
             img.Height = 50;
             tPart.Children.Add(img);
             Grid.SetColumn(img, (int)tile.Pos.X);
             Grid.SetRow(img, 7 - (int)tile.Pos.Y);
+        }
+
+        private BitmapImage TurnSwitch(string name)
+        {
+            return name.Contains("Left") == true ? (name.Contains("Up") == true ? _switches["SwitchTileLeftDown"] : _switches["SwitchTileLeftUp"]) : (name.Contains("Up") == true ? _switches["SwitchTileDownRight"] : _switches["SwitchTileUpRight"]);
+        }
+
+        private void Img_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Image img = (Image)sender;
+            img.Source = TurnSwitch(img.Source.ToString().Split('/').Last().Split('.').First());
+            img.UpdateLayout();
+        }
+
+        private void Img_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image img = (Image)sender;
+            img.Cursor = Cursors.Hand;
         }
 
         public void PlaceCart(int start)
@@ -65,13 +92,22 @@ namespace Goudkoorts
 
         }
 
+        private void initSwitches()
+        {
+            var f = Directory.GetFiles("../../Assets/Images","SwitchTile*");
+            for (int i = 0; i < f.Count(); i++)
+            {
+                string name = f[i].Split(new[] { "\\" }, StringSplitOptions.None).Last().Split('.').First();
+                _switches.Add(name, new BitmapImage(new Uri(f[i],UriKind.Relative)));
+            }
+        }
+
         private void initGrid(int xSize, int ySize)
         {
             for (int i = 0; i < xSize; i++)
             {
                 ColumnDefinition gridCol = new ColumnDefinition();
                 gridCol.Name = "x" + i;
-                gridCol.MouseMove += GridCol_MouseMove;
                 tPart.ColumnDefinitions.Add(gridCol);
             }
 
@@ -79,21 +115,8 @@ namespace Goudkoorts
             {
                 RowDefinition rowCol = new RowDefinition();
                 rowCol.Name = "y" + i;
-                rowCol.MouseMove += RowCol_MouseMove;
                 tPart.RowDefinitions.Add(rowCol);
             }
-        }
-
-        private void RowCol_MouseMove(object sender, MouseEventArgs e)
-        {
-            RowDefinition r = (RowDefinition)sender;
-            Console.WriteLine(r.Name);
-        }
-
-        private void GridCol_MouseMove(object sender, MouseEventArgs e)
-        {
-            ColumnDefinition c = (ColumnDefinition)sender;
-            Console.WriteLine(c.Name);
         }
     }
 }
